7cf4cd63e3746c8f17639ea1535dc0052d1e0597
Alef Daniel <alefsa_daniel@hotmail.com>
2021-09-02 23:02:15 -0300

    Creating project and git repository

---
+++ db/DbintegrityException.java
@@ -0,0 +1,9 @@
+package db;
+
+public class DbintegrityException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public DbintegrityException(String msg){
+        super(msg);
+    }
+}
