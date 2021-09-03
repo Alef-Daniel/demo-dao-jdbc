60e72c5976c88748aeb273383eec7a3341f48263
Alef Daniel <alefsa_daniel@hotmail.com>
2021-09-03 00:28:43 -0300

    findByID implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,12 +1,14 @@
 package model.dao;
 
+import model.entities.Seller;
+
 import java.util.List;
 
 public interface SellerDao {
 
-    void insert(SellerDao obj);
-    void update(SellerDao obj);
+    void insert(Seller obj);
+    void update(Seller obj);
     void deleteById(Integer id);
-    SellerDao findById(Integer id);
-    List<SellerDao> findAll();
+    Seller findById(Integer id);
+    List<Seller> findAll();
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,19 +1,33 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
 
+    Connection conn;
+
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
 
     @Override
-    public void insert(SellerDao obj) {
+    public void insert(Seller obj) {
 
     }
 
     @Override
-    public void update(SellerDao obj) {
+    public void update(Seller obj) {
 
     }
 
@@ -23,12 +37,43 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public SellerDao findById(Integer id) {
-        return null;
+    public Seller findById(Integer id) {
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            st = conn.prepareStatement("SELECT seller.*,department.Name as DepName " +
+                    "FROM seller INNER JOIN department " +
+                    "ON seller.DepartmentId = department.Id " +
+                    "WHERE seller.Id = ?");
+
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department department = new Department();
+                department.setId(rs.getInt("DepartmentId"));
+                department.setName(rs.getString("DepName"));
+                Seller seller = new Seller();
+                seller.setId(rs.getInt("Id"));
+                seller.setName(rs.getString("Name"));
+                seller.setEmail(rs.getString("Email"));
+                seller.setBaseSalary(rs.getDouble("BaseSalary"));
+                seller.setBirthDate(rs.getDate("BirthDate"));
+                seller.setDepartment(department);
+                return seller;
+
+            }
+            return null;
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
-    public List<SellerDao> findAll() {
+    public List<Seller> findAll() {
         return null;
     }
 }
