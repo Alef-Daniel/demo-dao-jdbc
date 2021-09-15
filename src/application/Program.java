872eb32607f34b6ad4231a89376be5fe43dffaae
Alef Daniel <alefsa_daniel@hotmail.com>
2021-09-14 23:37:01 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,7 +37,14 @@ public class Program {
             Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.00, department);
              sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = "+ newSeller .getId());
+
+        System.out.println("=== Test 4 Seller Update===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update Completed");
         }
+
 
     }
 
